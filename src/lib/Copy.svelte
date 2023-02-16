d851b02d1f40086423037536c553a53bcb45aa92
Kim HyunWoo <stories2@naver.com>
2023-02-16 21:45:34 +0900

    asf

--- src/lib/Copy.svelte
+++ src/lib/Copy.svelte
@@ -1,1 +1,21 @@
 <h1>Copy</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <pre><code>&lt;p&gt;Sample text here...&lt;/p&gt;
+                &lt;p&gt;And another line of sample text here...&lt;/p&gt;
+                </code></pre>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <button type="button" class="btn btn-outline-secondary">Copy</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            Copied!
+        </div>
+    </div>
+</div>
--- src/lib/Paste.svelte
+++ src/lib/Paste.svelte
@@ -1,14 +1,25 @@
 <h1>Paste</h1>
-<div class="container text-center">
-    <div class="row">
-      <div class="col">
-        Column
+<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-8 col-lg-7 col-xl-6">
+        <div class="input-group">
+            <textarea class="form-control" aria-label="With textarea" rows="3"></textarea>
+            <!-- <span class="input-group-text">With textarea</span> -->
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Paste</button>
+          </div> 
       </div>
-      <div class="col">
-        Column
-      </div>
-      <div class="col">
-        Column
-      </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-7 col-xl-6">
+            <label for="basic-url" class="form-label">Password</label>
+            <div class="input-group mb-3">
+                <input type="password" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col">
+            Pasted!
+        </div>
     </div>
   </div>
